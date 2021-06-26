ef88da83174b0a6af136f534df6c3f251017683e
Jung SongYi <lovelysi0113@gmail.com>
2021-06-26 23:53:25 +0900

    latest solved: 2021-06-26

---
+++ BOJ/05XXX/5337-웰컴.js
@@ -0,0 +1,15 @@
+/**
+ * ABOUT
+ *
+ * DATE: 2021-06-26
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://www.acmicpc.net/problem/5337
+ *
+ * COMMENT:
+ * 특수문자 따옴표(', "")나 역슬래시(\)를 출력하고 싶을 때 앞에 '\'를 붙이면 된다
+ **/
+
+console.log('.  .   .');
+console.log('|  | _ | _. _ ._ _  _');
+console.log('|/\\|(/.|(_.(_)[ | )(/.');
