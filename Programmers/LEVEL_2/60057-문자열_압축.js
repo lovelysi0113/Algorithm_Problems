78facf7d27d7d925b176866b8a53fc6fe64d4778
Jung SongYi <lovelysi0113@gmail.com>
2021-06-29 22:30:18 +0900

    latest solved: 2021-06-29 again

--- Programmers/LEVEL_2/60057-문자열_압축.js
+++ Programmers/LEVEL_2/60057-문자열_압축.js
@@ -1,55 +1,106 @@
 /**
-* ABOUT
-*
-* DATE: 2021-04-02
-* AUTHOR: lovelysi0113
-*
-* URL: https://programmers.co.kr/learn/courses/30/lessons/60057
-*
-* COMMENT:
-* 1~s.length 갯수만큼 문자열을 압축해보고 그 중 가장 짧은 길이의 압축된 문자열을 구한다
-**/
+ * ABOUT
+ *
+ * DATE: 2021-04-02
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://programmers.co.kr/learn/courses/30/lessons/60057
+ *
+ * COMMENT:
+ * 1~s.length 갯수만큼 문자열을 압축해보고 그 중 가장 짧은 길이의 압축된 문자열을 구한다
+ **/
 
 function solution(s) {
-    const MAX = s.length + 1;
-    let answer = 10000;
-    
-    // 1 ~ MAX 갯수만큼 문자열을 압축해보기
-    for(let rptNum = 1; rptNum < MAX; rptNum++) {
-        let nowAnswer = '';
-        let chkWord = '';
-        let cntWord = 0;
-        for (let idx = 0; idx < s.length+1; idx += rptNum) {
-            // 압축 시작하기
-            if (chkWord === '') {
-                chkWord = s.slice(idx, idx+rptNum);
-                cntWord = 1;
-            }
-            // 겹치는 문자열 있는지 확인
-            else if (chkWord === s.slice(idx, idx+rptNum)) {
-                cntWord += 1;
-            }
-            // 반복되지 않을 때
-            else {
-                if (cntWord === 1) {
-                    nowAnswer += chkWord;
-                } else {
-                    nowAnswer += cntWord + chkWord;
-                }
-                // 문자열이 끝나지 않았으면 계속 진행하기
-                if (idx !== s.length) {
-                    chkWord = s.slice(idx, idx+rptNum);
-                    cntWord = 1;
-                }
-            }
-        }
-        // 남은 자투리 문자열도 추가해주기
-        let last = s.length % rptNum;
-        if (last !== 0) nowAnswer += s.slice(-last);
-        
-        // 현재 압축한 문자열 길이가 최소길이인지 비교
-        answer = Math.min(answer, nowAnswer.length);
-    }
-    
-    return answer;
+	const MAX = s.length + 1;
+	let answer = 10000;
+
+	// 1 ~ MAX 갯수만큼 문자열을 압축해보기
+	for (let rptNum = 1; rptNum < MAX; rptNum++) {
+		let nowAnswer = '';
+		let chkWord = '';
+		let cntWord = 0;
+		for (let idx = 0; idx < s.length + 1; idx += rptNum) {
+			// 압축 시작하기
+			if (chkWord === '') {
+				chkWord = s.slice(idx, idx + rptNum);
+				cntWord = 1;
+			}
+			// 겹치는 문자열 있는지 확인
+			else if (chkWord === s.slice(idx, idx + rptNum)) {
+				cntWord += 1;
+			}
+			// 반복되지 않을 때
+			else {
+				if (cntWord === 1) {
+					nowAnswer += chkWord;
+				} else {
+					nowAnswer += cntWord + chkWord;
+				}
+				// 문자열이 끝나지 않았으면 계속 진행하기
+				if (idx !== s.length) {
+					chkWord = s.slice(idx, idx + rptNum);
+					cntWord = 1;
+				}
+			}
+		}
+		// 남은 자투리 문자열도 추가해주기
+		let last = s.length % rptNum;
+		if (last !== 0) nowAnswer += s.slice(-last);
+
+		// 현재 압축한 문자열 길이가 최소길이인지 비교
+		answer = Math.min(answer, nowAnswer.length);
+	}
+
+	return answer;
+}
+
+// **************************************************************************************************** //
+
+/**
+ * ABOUT
+ * DATE: 2021-06-29
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://programmers.co.kr/learn/courses/30/lessons/60057
+ *
+ * COMMENT:
+ * 코딩테스트 준비로 다시 한번 풀어보았다
+ **/
+
+function solution(s) {
+	let MAX_LEN = s.length + 1;
+	let answer = MAX_LEN;
+
+	for (let cut = 1; cut < MAX_LEN; cut++) {
+		let press = '';
+
+		let cnt = 0;
+		let word = '';
+		for (let idx = 0; idx < MAX_LEN; idx += cut) {
+			let chkword = s.slice(idx, idx + cut);
+			if (word === '') {
+				word = chkword;
+				cnt = 1;
+			} else if (word === chkword) {
+				cnt += 1;
+			} else {
+				if (cnt === 1) {
+					press += word;
+				} else {
+					press += cnt + word;
+				}
+				if (idx !== MAX_LEN) {
+					word = chkword;
+					cnt = 1;
+				}
+			}
+		}
+
+		let last = (MAX_LEN - 1) % cut;
+		if (last !== 0) press += s.slice(-last);
+
+		answer = Math.min(answer, press.length);
+	}
+
+	return answer;
 }
--- Programmers/LEVEL_2/72411-메뉴_리뉴얼.js
+++ Programmers/LEVEL_2/72411-메뉴_리뉴얼.js
@@ -1,71 +1,137 @@
 /**
-* ABOUT
-*
-* DATE: 2021-04-02
-* AUTHOR: lovelysi0113
-*
-* URL: https://programmers.co.kr/learn/courses/30/lessons/72411
-*
-* COMMENT:
-* course에 나온 갯수별로 각각 주문이 가장 많은 메뉴 조합을 찾는다
-* 손님이 시킨 메뉴에서 나올 수 있는 모든 조합을 구한 후 해당 조합이 몇번 나오는지 센다
-**/
+ * ABOUT
+ *
+ * DATE: 2021-04-02
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://programmers.co.kr/learn/courses/30/lessons/72411
+ *
+ * COMMENT:
+ * course에 나온 갯수별로 각각 주문이 가장 많은 메뉴 조합을 찾는다
+ * 손님이 시킨 메뉴에서 나올 수 있는 모든 조합을 구한 후 해당 조합이 몇번 나오는지 센다
+ **/
 
 function solution(orders, course) {
-    let answer = [];
-    
-    // course의 단품메뉴들의 갯수별로 계산하기
-    course.forEach(num => {
-        let menuComb = [];
-        orders.forEach(order => {
-            menuComb = menuComb.concat(getComb(order.split(''), num));
-        });
-        
-        // 메뉴 조합 중 최대갯수 구하기
-        let menuCount = {};
-        let maxNum = 1;
-        menuComb.forEach(menu => {
-            if (menuCount[menu] === undefined) {
-                menuCount[menu] = 1;
-            } else {
-                menuCount[menu] += 1;
-                if (menuCount[menu] > maxNum) {
-                    maxNum = menuCount[menu];
-                }
-            }
-        });
+	let answer = [];
 
-        // 최대갯수인 조합을 answer에 넣기
-        answer = answer.concat(Object.keys(menuCount).filter(key => {
-            if (menuCount[key] === maxNum && maxNum > 1) return true;
-        }));
-    });
-    
-    // 오름차순 정렬
-    return answer.sort((a,b) => order(a, b));
+	// course의 단품메뉴들의 갯수별로 계산하기
+	course.forEach(num => {
+		let menuComb = [];
+		orders.forEach(order => {
+			menuComb = menuComb.concat(getComb(order.split(''), num));
+		});
+
+		// 메뉴 조합 중 최대갯수 구하기
+		let menuCount = {};
+		let maxNum = 1;
+		menuComb.forEach(menu => {
+			if (menuCount[menu] === undefined) {
+				menuCount[menu] = 1;
+			} else {
+				menuCount[menu] += 1;
+				if (menuCount[menu] > maxNum) {
+					maxNum = menuCount[menu];
+				}
+			}
+		});
+
+		// 최대갯수인 조합을 answer에 넣기
+		answer = answer.concat(
+			Object.keys(menuCount).filter(key => {
+				if (menuCount[key] === maxNum && maxNum > 1) return true;
+			}),
+		);
+	});
+
+	// 오름차순 정렬
+	return answer.sort((a, b) => order(a, b));
 }
 
 // 조합 구하는 함수
 function getComb(arr, num) {
-    let result = [];
-    if (num === 1) return arr;
-    arr.forEach((select, idx, origin) => {
-        // select+1 ~ 마지막 부분에서 나머지 조합 선택
-        let rest = origin.slice(idx+1);
-        let restComb = getComb(rest, num-1);
-        // select와 합치기
-        let add = restComb.map(comb => {
-            return [select, ...comb].sort((a,b) => order(a, b)).join('');
-        });
-        // result에 현재 나온 조합 추가
-        result.push(...add);
-    });
-    return result;
+	let result = [];
+	if (num === 1) return arr;
+	arr.forEach((select, idx, origin) => {
+		// select+1 ~ 마지막 부분에서 나머지 조합 선택
+		let rest = origin.slice(idx + 1);
+		let restComb = getComb(rest, num - 1);
+		// select와 합치기
+		let add = restComb.map(comb => {
+			return [select, ...comb].sort((a, b) => order(a, b)).join('');
+		});
+		// result에 현재 나온 조합 추가
+		result.push(...add);
+	});
+	return result;
 }
 
 // 문자열 오름차순 정렬
 function order(a, b) {
-    if (a > b) return 1;
-    if (a === b) return 0;
-    if (a < b) return -1;
+	if (a > b) return 1;
+	if (a === b) return 0;
+	if (a < b) return -1;
+}
+
+// **************************************************************************************************** //
+
+/**
+ * ABOUT
+ * DATE: 2021-06-29
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://programmers.co.kr/learn/courses/30/lessons/72411
+ *
+ * COMMENT:
+ * 코딩테스트 준비로 다시 한번 풀어보았다
+ **/
+
+function solution(orders, course) {
+	let answer = [];
+
+	course.forEach(num => {
+		let menus = orders.reduce((arr, order) => {
+			order = order.split('').sort((a, b) => {
+				if (a > b) return 1;
+				else if (a < b) return -1;
+				else return 0;
+			});
+			return [...arr, ...combination(order, num)];
+		}, []);
+		let cntMenu = {};
+		let maxCnt = 2;
+		let maxMenu = [];
+		menus.forEach(menu => {
+			if (cntMenu[menu] === undefined) {
+				cntMenu[menu] = 0;
+			}
+			cntMenu[menu] += 1;
+			if (maxCnt < cntMenu[menu]) {
+				maxCnt = cntMenu[menu];
+				maxMenu = [menu];
+			} else if (maxCnt === cntMenu[menu]) {
+				maxMenu.push(menu);
+			}
+		});
+		answer = [...answer, ...maxMenu];
+	});
+
+	return answer.sort((a, b) => {
+		if (a > b) return 1;
+		else if (a < b) return -1;
+		else return 0;
+	});
+}
+
+function combination(foods, select) {
+	if (select === 1) {
+		return foods;
+	}
+	let result = [];
+	for (let idx = 0; idx < foods.length; idx++) {
+		let now = foods[idx];
+		let rest = combination(foods.slice(idx + 1), select - 1);
+		let addrest = rest.map(el => now + el);
+		result = [...result, ...addrest];
+	}
+	return result;
 }
