216f8fe56b36050ee992ae5f0d0037560b5db5ca
Jung SongYi <lovelysi0113@gmail.com>
2021-07-01 14:54:11 +0900

    latest solved: 2021-07-01 again

--- Programmers/LEVEL_2/17679-프렌즈4블록.js
+++ Programmers/LEVEL_2/17679-프렌즈4블록.js
@@ -14,51 +14,114 @@
  **/
 
 function solution(m, n, board) {
-  board = board.map((el) => el.split(""));
+	board = board.map(el => el.split(''));
 
-  // 블록 지우기
-  while (true) {
-    // 지울 수 있는 4개 블록 찾기
-    let deleteArr = [];
-    for (let x = 0; x < m - 1; x++) {
-      for (let y = 0; y < n - 1; y++) {
-        if (
-          board[x][y] !== 0 &&
-          board[x][y] === board[x][y + 1] &&
-          board[x][y] === board[x + 1][y] &&
-          board[x][y] === board[x + 1][y + 1]
-        ) {
-          deleteArr.push([x, y]);
-        }
-      }
-    }
-    if (deleteArr.length === 0) {
-      return board
-        .reduce((arr, el) => arr.concat(el), [])
-        .filter((el) => el === 0).length;
-    } else {
-      // 없앤 블록 표시
-      deleteArr.forEach((el) => {
-        let [x, y] = el;
-        board[x][y] = 0;
-        board[x][y + 1] = 0;
-        board[x + 1][y] = 0;
-        board[x + 1][y + 1] = 0;
-      });
-    }
-    // 빈 공간 채우기
-    for (let x = m - 1; x > 0; x--) {
-      if (board[x].filter((el) => el === 0).length === 0) {
-        continue;
-      }
-      for (let y = 0; y < n; y++) {
-        for (let move = x - 1; move >= 0; move--) {
-          if (board[x][y] === 0 && board[move][y] !== 0) {
-            [board[x][y], board[move][y]] = [board[move][y], board[x][y]];
-            break;
-          }
-        }
-      }
-    }
-  }
+	// 블록 지우기
+	while (true) {
+		// 지울 수 있는 4개 블록 찾기
+		let deleteArr = [];
+		for (let x = 0; x < m - 1; x++) {
+			for (let y = 0; y < n - 1; y++) {
+				if (
+					board[x][y] !== 0 &&
+					board[x][y] === board[x][y + 1] &&
+					board[x][y] === board[x + 1][y] &&
+					board[x][y] === board[x + 1][y + 1]
+				) {
+					deleteArr.push([x, y]);
+				}
+			}
+		}
+		if (deleteArr.length === 0) {
+			return board.reduce((arr, el) => arr.concat(el), []).filter(el => el === 0).length;
+		} else {
+			// 없앤 블록 표시
+			deleteArr.forEach(el => {
+				let [x, y] = el;
+				board[x][y] = 0;
+				board[x][y + 1] = 0;
+				board[x + 1][y] = 0;
+				board[x + 1][y + 1] = 0;
+			});
+		}
+		// 빈 공간 채우기
+		for (let x = m - 1; x > 0; x--) {
+			if (board[x].filter(el => el === 0).length === 0) {
+				continue;
+			}
+			for (let y = 0; y < n; y++) {
+				for (let move = x - 1; move >= 0; move--) {
+					if (board[x][y] === 0 && board[move][y] !== 0) {
+						[board[x][y], board[move][y]] = [board[move][y], board[x][y]];
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+// **************************************************************************************************** //
+
+/**
+ * ABOUT
+ * DATE: 2021-07-01
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://programmers.co.kr/learn/courses/30/lessons/17679
+ *
+ * COMMENT:
+ * 코딩테스트 준비로 다시 한번 풀어보았다
+ **/
+
+function solution(m, n, board) {
+	board = board.map(el => el.split(''));
+
+	while (true) {
+		let canDelete = false;
+
+		// 지워지는 블록 찾기
+		let position = [];
+		for (let x = 0; x < m - 1; x++) {
+			for (let y = 0; y < n - 1; y++) {
+				if (
+					board[x][y] !== '0' &&
+					board[x][y] === board[x + 1][y] &&
+					board[x][y] === board[x][y + 1] &&
+					board[x][y] === board[x + 1][y + 1]
+				) {
+					position.push([x, y]);
+					canDelete = true;
+				}
+			}
+		}
+
+		// 지워지는 블록 없음 -> 종료
+		if (!canDelete) {
+			return board.reduce((sum, line) => sum + line.filter(block => block === '0').length, 0);
+		}
+
+		// 블록 지우기('0'으로 변경)
+		while (position.length > 0) {
+			let [x, y] = position.pop();
+			board[x][y] = '0';
+			board[x + 1][y] = '0';
+			board[x][y + 1] = '0';
+			board[x + 1][y + 1] = '0';
+		}
+
+		// 빈 공간 채우기
+		for (let x = m - 1; x > 0; x--) {
+			for (let y = 0; y < n; y++) {
+				if (board[x][y] === '0') {
+					for (let chk = x - 1; chk >= 0; chk--) {
+						if (board[chk][y] !== '0') {
+							[board[x][y], board[chk][y]] = [board[chk][y], board[x][y]];
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- Programmers/LEVEL_2/17683-방금그곡.js
+++ Programmers/LEVEL_2/17683-방금그곡.js
@@ -14,46 +14,99 @@
  **/
 
 function solution(m, musicinfos) {
-  m = melody(m);
+	m = melody(m);
 
-  // 음악 정보를 토대로 해당 음악의 재생시간, 멜로디 구하기
-  musicinfos = musicinfos.map((music) => {
-    music = music.split(",");
-    // 음악 재생 시간
-    let HH = music[1].substring(0, 2) - music[0].substring(0, 2);
-    let MM = music[1].substring(3) - music[0].substring(3);
-    let time = HH * 60 + MM;
-    // 재생된 멜로디
-    let note = melody(music[3]);
-    let play = "";
-    let idx = 0;
-    while (idx < time) {
-      play += note;
-      idx += note.length;
-    }
-    play += note.substring(0, time - idx);
-    play = play.substring(0, time);
-    return [time, music[2], play];
-  });
+	// 음악 정보를 토대로 해당 음악의 재생시간, 멜로디 구하기
+	musicinfos = musicinfos.map(music => {
+		music = music.split(',');
+		// 음악 재생 시간
+		let HH = music[1].substring(0, 2) - music[0].substring(0, 2);
+		let MM = music[1].substring(3) - music[0].substring(3);
+		let time = HH * 60 + MM;
+		// 재생된 멜로디
+		let note = melody(music[3]);
+		let play = '';
+		let idx = 0;
+		while (idx < time) {
+			play += note;
+			idx += note.length;
+		}
+		play += note.substring(0, time - idx);
+		play = play.substring(0, time);
+		return [time, music[2], play];
+	});
 
-  let answer = [0, "(None)"];
-  musicinfos.forEach((music) => {
-    let [time, title, play] = music;
-    // 멜로디(m)가 음악(play)에 포함되어 있는지 확인
-    if (play.includes(m) && time > answer[0]) {
-      answer = [time, title];
-    }
-  });
+	let answer = [0, '(None)'];
+	musicinfos.forEach(music => {
+		let [time, title, play] = music;
+		// 멜로디(m)가 음악(play)에 포함되어 있는지 확인
+		if (play.includes(m) && time > answer[0]) {
+			answer = [time, title];
+		}
+	});
 
-  return answer[1];
+	return answer[1];
 }
 
-const melody = (song) => {
-  // 음 중 #이 있는 음은 소문자로 치환
-  return song
-    .replace(/(C#)/g, "c")
-    .replace(/(D#)/g, "d")
-    .replace(/(F#)/g, "f")
-    .replace(/(G#)/g, "g")
-    .replace(/(A#)/g, "a");
+const melody = song => {
+	// 음 중 #이 있는 음은 소문자로 치환
+	return song
+		.replace(/(C#)/g, 'c')
+		.replace(/(D#)/g, 'd')
+		.replace(/(F#)/g, 'f')
+		.replace(/(G#)/g, 'g')
+		.replace(/(A#)/g, 'a');
 };
+
+// **************************************************************************************************** //
+
+/**
+ * ABOUT
+ * DATE: 2021-07-01
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://programmers.co.kr/learn/courses/30/lessons/17683
+ *
+ * COMMENT:
+ * 코딩테스트 준비로 다시 한번 풀어보았다
+ **/
+
+function solution(m, musicinfos) {
+	m = setmelody(m);
+	let answer = { maxtime: 0, start: '2359', title: '(None)' };
+	musicinfos.forEach(musicinfo => {
+		let [start, end, title, music] = musicinfo.split(',');
+		let playtime = settime(start, end);
+		let playmusic = setmusic(playtime, music);
+		if (playmusic.includes(m)) {
+			let chkstart = start.slice(0, 2) + start.slice(3);
+			if (answer.maxtime < playtime || (answer.maxtime === playtime && answer.start - chkstart > 0)) {
+				answer.maxtime = playtime;
+				answer.start = chkstart;
+				answer.title = title;
+			}
+		}
+	});
+	return answer.title;
+}
+
+function setmelody(str) {
+	return str.replace(/C#/g, 'c').replace(/D#/g, 'd').replace(/F#/g, 'f').replace(/G#/g, 'g').replace(/A#/g, 'a');
+}
+
+function settime(start, end) {
+	let [sh, sm] = start.split(':').map(el => parseInt(el));
+	let [eh, em] = end.split(':').map(el => parseInt(el));
+	return (eh - sh) * 60 + (em - sm);
+}
+
+function setmusic(time, music) {
+	music = setmelody(music);
+	let result = '';
+	let num = 0;
+	while (num <= time) {
+		result += music;
+		num += music.length;
+	}
+	return result.slice(0, time);
+}
