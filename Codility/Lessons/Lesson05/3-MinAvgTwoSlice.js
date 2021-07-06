5a2fda3ad306eab9b05a0a1247c013bae342fd30
Jung SongYi <lovelysi0113@gmail.com>
2021-07-06 19:31:16 +0900

    latest solved: 2021-07-06 again

--- Codility/Lessons/Lesson05/3-MinAvgTwoSlice.js
+++ Codility/Lessons/Lesson05/3-MinAvgTwoSlice.js
@@ -38,3 +38,44 @@ function solution(A) {
 
 	return minIdx;
 }
+
+// **************************************************************************************************** //
+
+/**
+ * ABOUT
+ * DATE: 2021-07-06
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
+ *
+ * COMMENT:
+ * 귀류법을 통해 2~3개의 평균을 구해 그 중에서 최소값을 찾으면 되는 것을 알 수 있다
+ * 최소 평균인 구간의 길이가 4라고 가정한다
+ * 해당 구간의 수를 a, b, c, d라고 한다면 a, b, c, d의 평균은 (a, b)의 평균과 (c, d)의 평균이다
+ * 만약 두 구간의 평균이 같다면((a, b, c, d) = (a, b)) 답은 시작 인덱스인 a의 인덱스가 된다
+ * 만약 두 구간 중 한쪽의 구간의 평균이 더 작다면 작은 구간을 택해야 평균이 낮아지므로 최소 평균 구간이 (a, b, c, d)가 될 수 없다
+ * 따라서 평균이 최소가 되는 구간의 길이는 4일 수 없다
+ * 이후 2개, 3개씩만 비교하면서 문제를 풀면 된다
+ **/
+
+function solution(A) {
+	let startIdx = 0;
+	let minAvg = (A[0] + A[1]) / 2;
+
+	for (let idx = 2; idx < A.length; idx++) {
+		// 2개의 평균 구하기
+		let avg1 = (A[idx] + A[idx - 1]) / 2;
+		if (avg1 < minAvg) {
+			minAvg = avg1;
+			startIdx = idx - 1;
+		}
+		// 3개의 평균 구하기
+		let avg2 = (A[idx] + A[idx - 1] + A[idx - 2]) / 3;
+		if (avg2 < minAvg) {
+			minAvg = avg2;
+			startIdx = idx - 2;
+		}
+	}
+
+	return startIdx;
+}
