649473f006444210ddaddffe56a63f5f638e6a6e
Jung SongYi <lovelysi0113@gmail.com>
2021-07-14 23:40:53 +0900

    latest solved: 2021-07-14 again

--- Codility/Lessons/Lesson01/1-BinaryGap.js
+++ Codility/Lessons/Lesson01/1-BinaryGap.js
@@ -36,3 +36,44 @@ function solution(N) {
 	}
 	return answer;
 }
+
+// **************************************************************************************************** //
+
+/**
+ * ABOUT
+ * DATE: 2021-07-14
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+ *
+ * COMMENT:
+ * 코딩테스트 준비로 다시 한번 풀어보았다
+ **/
+
+function solution(N) {
+	// binary gap: 양의 정수 N을 2진수로 변환했을 때, 1과 1 사이에 있는 0의 갯수 중 가장 긴 길이
+	// binary gap 찾는 문제
+
+	// N을 2진수로 변환하면서 그때마다 숫자를 확인해준다
+	// 1인 경우 -> binary gap 끝, 최댓값 찾기
+	// 0인 경우 -> binary gap 길이 추가
+
+	let maxGap = 0; // 가장 긴 binary gap의 길이
+
+	let startOne = false;
+	let cnt = 0; // binary gap이 나올 때마다 길이 체크하기
+	while (N > 0) {
+		let now = N % 2;
+		if (now === 1) {
+			startOne = true;
+			maxGap = Math.max(maxGap, cnt);
+			cnt = 0; // binary gap 초기화
+		}
+		if (startOne && now === 0) {
+			cnt += 1;
+		}
+		N = Math.floor(N / 2);
+	}
+
+	return maxGap;
+}
