3ec4f5cdba6c96ff885196f39394b6d579c7c9a6
Jung SongYi <lovelysi0113@gmail.com>
2021-08-16 12:17:06 +0900

    latest solved: 2021-08-16

---
+++ BOJ/02XXX/2530-인공지능_시계.js
@@ -0,0 +1,41 @@
+/**
+ * ABOUT
+ *
+ * DATE: 2021-08-16
+ * AUTHOR: lovelysi0113
+ *
+ * URL: https://www.acmicpc.net/problem/2530
+ *
+ * COMMENT:
+ * 현재 시각에서 요리 시간을 더한 후의 시각을 구하면 된다
+ * 단, 23시 59분에서 1분이 지나면 0시 0분이 되는 것에 주의해야 한다
+ **/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+	input.push(line);
+}).on('close', function () {
+	let [H, M, S] = input[0].split(' ').map(el => parseInt(el));
+	let cook = parseInt(input[1]);
+
+	S += cook;
+	M += parseInt(S / 60);
+	S = S % 60;
+	H += parseInt(M / 60);
+	if (H > 23) {
+		H = H % 24;
+	}
+	M = M % 60;
+
+	console.log(H, M, S);
+
+	process.exit();
+});
